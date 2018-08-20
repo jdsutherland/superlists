92b87121c8983ea51503bf2a949fd1c693b8a58d
Jeff Sutherland <jeffdsuth@gmail.com>
2018-08-20 13:28:29 +0700

    Show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,7 @@ from lists.models import Item
 
 
 class HomePageTest(TestCase):
-    def test_useshome_template(self):
+    def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
@@ -16,9 +16,30 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
 
     def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        self.assertEqual(Item.objects.first().text, 'A new list item')
+
+    def test_redirects_after_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_item_when_necessary(self):
+        self.client.get('/')
+
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemy 1')
+        Item.objects.create(text='itemy 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemy 1', response.content.decode())
+        self.assertIn('itemy 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from lists.models import Item
+from django.shortcuts import render, redirect
 
 home_page = None
 
 
 def home_page(req):
-    return render(req, 'home.html', {
-        'new_item_text': req.POST.get('item_text', ''),
-    })
+    if req.method == 'POST':
+        Item.objects.create(text=req.POST['item_text'])
+        return redirect('/')
+
+    items = Item.objects.all()
+    return render(req, 'home.html', {'items': items})
