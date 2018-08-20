aaef9496aa1f9c6fd76b2d4f38782eef15061fa6
Jeff Sutherland <jeffdsuth@gmail.com>
2018-08-20 09:59:10 +0700

    WIP refactor

--- functional_tests.py
+++ functional_tests.py
@@ -17,6 +17,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_list_in_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retreive_it_later(self):
         # visit homepage
         self.browser.get('http://localhost:8000')
@@ -35,16 +40,21 @@ class NewVisitorTest(unittest.TestCase):
         # when she hits ENTER, the page updates & the page lists the to-do list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in table'
-        )
+        # There is still a text box inviting her to add another item. She
+        # enters "Use peacock feathers to make a fly" (Edith is very
+        # methodical)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        self.check_for_row_list_in_table('1: Buy peacock feathers')
+        self.check_for_row_list_in_table(
+            '2: Use peacock feathers to make a fly')
 
         # There is still a textbox inviting her to add another item.
         #   She enters 'Use peacock feathers to make fly'
-        self.assertFail('Finish the test!')
+        self.fail('Finish the test!')
 
 
 if __name__ == "__main__":
--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 home_page = None
 
 
 def home_page(req):
-    return render(req, 'home.html')
+    return render(req, 'home.html', {
+        'new_item_text': req.POST.get('item_text', ''),
+    })
