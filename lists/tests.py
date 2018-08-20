548bc655af384f77cc6de58c9230d06f20db63b4
Jeff Sutherland <jeffdsuth@gmail.com>
2018-08-20 15:23:58 +0700

    Add new list view

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+
 from lists.models import Item
 
 
@@ -7,22 +8,24 @@ class HomePageTest(TestCase):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
 
+    def test_only_saves_item_when_necessary(self):
+        self.client.get('/')
+
+        self.assertEqual(Item.objects.count(), 0)
+
+
+class NewListTest(TestCase):
     def test_can_save_a_POST_request(self):
-        self.client.post('/', data={'item_text': 'A new list item'})
+        self.client.post('/lists/new', data={'item_text': 'A new list item'})
 
         self.assertEqual(Item.objects.count(), 1)
         self.assertEqual(Item.objects.first().text, 'A new list item')
 
     def test_redirects_after_POST_request(self):
-        response = self.client.post('/', data={'item_text': 'A new list item'})
+        response = self.client.post(
+            '/lists/new', data={'item_text': 'A new list item'})
 
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list/')
-
-    def test_only_saves_item_when_necessary(self):
-        self.client.get('/')
-
-        self.assertEqual(Item.objects.count(), 0)
+        self.assertRedirects(response, '/lists/the-only-list/')
 
 
 class ListViewTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -5,13 +5,14 @@ home_page = None
 
 
 def home_page(req):
-    if req.method == 'POST':
-        Item.objects.create(text=req.POST['item_text'])
-        return redirect('/lists/the-only-list/')
-
     return render(req, 'home.html')
 
 
 def view_list(req):
     items = Item.objects.all()
     return render(req, 'list.html', {'items': items})
+
+
+def new_list(req):
+    Item.objects.create(text=req.POST['item_text'])
+    return redirect('/lists/the-only-list/')
