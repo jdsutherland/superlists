90e3894d269e4dd8abcf871f3cbd77eb35748097
Jeff Sutherland <jeffdsuth@gmail.com>
2018-08-20 07:31:32 +0700

    Add homepage test

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 home_page = None
 
 
-def home_page():
-    pass
+def home_page(req):
+    return HttpResponse('<html><title>To-Do lists</title></html>')
